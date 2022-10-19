f3c62e58bd343b7fd6405932b20bef0e77d0de0c
Michael Shepherd <michaelshep52@gmail.com>
2022-10-18 21:03:28 -0400

    Cleaning up unused code.

--- SM.MovieDataApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SM.MovieDataApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
